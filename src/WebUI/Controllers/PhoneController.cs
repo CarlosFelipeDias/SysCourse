f1e07f17f12142f98e9ee29225aaf4abe07ea7dc
CarlosFelipeDias <carlosfelipedias@gmail.com>
2026-04-06 15:20:32 -0300

    Create the Phone anctroler and folder to the PhoneViews

---
+++ src/WebUI/Controllers/PhoneController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using AutoMapper;
+using WebUI.Models;
+using DAO;
+using DTO;
+using DAO.Interfaces;
+
+namespace WebUI.Controllers
+{
+    //[Route("[controller]")]
+    public class PhoneController : Controller
+    {
+        // private readonly ILogger<PhoneController> _logger;
+
+        // public PhoneController(ILogger<PhoneController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        private readonly IPhoneDAO _phoneDAO;
+        private readonly IMapper _mapper;   
+        
+        public PhoneController(IPhoneDAO phoneDAO, IMapper mapper)
+        {
+            _phoneDAO = phoneDAO;
+            _mapper = mapper;
+        }   
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
